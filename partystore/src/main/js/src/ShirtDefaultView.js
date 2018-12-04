58880bfeaa22fecb26731e8889755bdf9c91a913
NAVEEN SAI KRISHNA AMULURU <namuluru@uncc.edu>
2018-12-04 00:09:09 -0500

    shirt alignments

--- partystore/src/main/js/src/ShirtDefaultView.js
+++ partystore/src/main/js/src/ShirtDefaultView.js
@@ -11,60 +11,131 @@ export class ShirtDefaultView extends React.Component{
 <div class="row">
 <div class="column">
     <div class="card">
+    <div class="flip-box">
+     <div class="flip-box-inner">
+       <div class="flip-box-front">
       <img src={require('./images/redshirt.jpg')} alt="cakes" style={{width:'100%'}}/>
       <h4>RED T-SHIRT</h4>
       <p class="price">$19.99</p>
-      <p>RED T-SHIRT</p>
         <p><button>ADD TO CART</button></p>
   </div>
+  <div class="flip-box-back">
+  <h4>RED T-SHIRT</h4>
+  <img src={require('./images/redshirt.jpg')} alt="cakes" style={{width:'100%'}}/>
+  <br></br>
+  <b><p>SIZE AVAILABLE</p></b>
+  <b><p>L</p></b>
   </div>
+  </div>
+</div>
+</div>
+</div>
 <div class="column">
     <div class="card">
+    <div class="flip-box">
+     <div class="flip-box-inner">
+       <div class="flip-box-front">
       <img src={require('./images/whiteshirt.jpg')} alt="cakes" style={{width:'100%'}}/>
       <h4>WHITE T-SHIRT</h4>
       <p class="price">$19.99</p>
-      <p>WHITE T-SHIRT</p>
         <p><button>ADD TO CART</button></p>
-  </div>
-  </div>
+        </div>
+        <div class="flip-box-back">
+        <h4>WHITE T-SHIRT</h4>
+        <img src={require('./images/whiteshirt.jpg')} alt="cakes" style={{width:'100%'}}/>
+        <br></br>
+        <b><p>SIZE AVAILABLE</p></b>
+        <b><p>L</p></b>
+        </div>
+        </div>
+      </div>
+      </div>
+      </div>
 <div class="column">
     <div class="card">
+    <div class="flip-box">
+     <div class="flip-box-inner">
+       <div class="flip-box-front">
       <img src={require('./images/greenshirt.jpg')}alt="cakes" style= {{width:'100%'}}/>
       <h4>GREEN T-SHIRT</h4>
       <p class="price">$19.99</p>
-      <p>GREEN T-SHIRT</p>
         <p><button>ADD TO CART</button></p>
-  </div>
-  </div>
+        </div>
+        <div class="flip-box-back">
+        <h4>GREEN T-SHIRT</h4>
+          <img src={require('./images/greenshirt.jpg')}alt="cakes" style= {{width:'100%'}}/>
+        <br></br>
+        <b><p>SIZE AVAILABLE</p></b>
+        <b><p>L</p></b>
+        </div>
+        </div>
+      </div>
+      </div>
+      </div>
 <div class="column">
     <div class="card">
+    <div class="flip-box">
+     <div class="flip-box-inner">
+       <div class="flip-box-front">
       <img src={require('./images/blackshirt.jpg')} alt="cakes" style={{width:'100%'}}/>
       <h4>BLACK T-SHIRT</h4>
       <p class="price">$19.99</p>
-      <p>BLACK T-SHIRT</p>
         <p><button>ADD TO CART</button></p>
-</div>
-  </div>
+        </div>
+        <div class="flip-box-back">
+        <h4>BLACK T-SHIRT</h4>
+          <img src={require('./images/blackshirt.jpg')} alt="cakes" style={{width:'100%'}}/>
+        <br></br>
+        <b><p>SIZE AVAILABLE</p></b>
+        <b><p>L</p></b>
+        </div>
+        </div>
+      </div>
+      </div>
+      </div>
 <div class="column">
     <div class="card">
+    <div class="flip-box">
+     <div class="flip-box-inner">
+       <div class="flip-box-front">
       <img src={require('./images/skyblueshirt.png')} alt="cakes" style={{width:'100%'}}/>
       <h4>BLUE T-SHIRT</h4>
       <p class="price">$19.99</p>
-      <p>BLUE T-SHIRT</p>
         <p><button>ADD TO CART</button></p>
-</div>
-  </div>
-
+        </div>
+        <div class="flip-box-back">
+        <h4>BLUE T-SHIRT</h4>
+          <img src={require('./images/skyblueshirt.png')} alt="cakes" style={{width:'100%'}}/>
+        <br></br>
+        <b><p>SIZE AVAILABLE</p></b>
+        <b><p>L</p></b>
+        </div>
+        </div>
+      </div>
+      </div>
+      </div>
 <div class="column">
     <div class="card">
+    <div class="flip-box">
+     <div class="flip-box-inner">
+       <div class="flip-box-front">
       <img src={require('./images/yellowshirt.jpg')} alt="cakes" style={{width:'100%'}}/>
       <h4>YELLOW T-SHIRT</h4>
       <p class="price">$19.99</p>
-      <p>YELLOW T-SHIRT</p>
         <p><button>ADD TO CART</button></p>
 
-  </div>
-  </div>
+        </div>
+        <div class="flip-box-back">
+        <h4>YELLOW T-SHIRT</h4>
+        <img src={require('./images/yellowshirt.jpg')} alt="cakes" style={{width:'100%'}}/>
+        <br></br>
+        <b><p>SIZE AVAILABLE</p></b>
+        <b><p>L</p></b>
+        </div>
+        </div>
+      </div>
+      </div>
+      </div>
 
   </div>
 
